d9dd10e3bd8eb829bf619199f16f876701f16b01
Rads <45115368+radsn@users.noreply.github.com>
2024-09-21 22:23:04 +0000

    added different dynamic paths

--- dice.rb
+++ dice.rb
@@ -9,13 +9,14 @@ BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
 
 get '/' do
   "Hello World"
-  "<h1>2d6</h1>
-  <ul>
-    <li><a href=\"/dice/2/6\">Roll two 6-sided dice</a></li>
-    <li><a href=\"/dice/2/10\">Roll two 10-sided dice</a></li>
-    <li><a href=\"/dice/1/20\">Roll one 20-sided die</a></li>
-    <li><a href=\"/dice/5/4\">Roll five 4-sided dice</a></li>
-  </ul>"
+  erb(:elephant)
+  # "<h1>2d6</h1>
+  # <ul>
+  #   <li><a href=\"/dice/2/6\">Roll two 6-sided dice</a></li>
+  #   <li><a href=\"/dice/2/10\">Roll two 10-sided dice</a></li>
+  #   <li><a href=\"/dice/1/20\">Roll one 20-sided die</a></li>
+  #   <li><a href=\"/dice/5/4\">Roll five 4-sided dice</a></li>
+  # </ul>"
 end
 
 get("/zebra") do
@@ -26,10 +27,11 @@ get("/dice/2/6") do
   first_die = rand(1..6)
   second_die = rand(1..6)
   sum = first_die + second_die
-  outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
-  	
-  "<h1>2d6</h1>
-   <p>#{outcome}</p>"
+  #outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+  @outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+  # "<h1>2d6</h1>
+  #  <p>#{outcome}</p>"
+  erb(:two_six, {:layout => :wrapper})
 end 
 
 
@@ -37,19 +39,21 @@ get("/dice/2/10") do
   first_die = rand(1..10)
   second_die = rand(1..10)
   sum = first_die + second_die
-  outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+  @outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
   	
-  "<h1>2d10</h1>
-   <p>#{outcome}</p>"
+  # "<h1>2d10</h1>
+  #  <p>#{outcome}</p>"
+  erb(:two_ten, {:layout => :wrapper})
 end 
 
 
 get("/dice/1/20") do
-  first_die = rand(1..20)
-  outcome = "You rolled a #{first_die}."
+  @first_die = rand(1..20)
+  @outcome = "You rolled a #{first_die}."
   	
-  "<h1>1d20</h1>
-   <p>#{outcome}</p>"
+  # "<h1>1d20</h1>
+  #  <p>#{outcome}</p>"
+  erb(:one_twenty, {:layout => :wrapper})
 end 
 
 
@@ -59,9 +63,22 @@ get("/dice/5/4") do
   third_die = rand(1..4)
   fourth_die = rand(1..4)
   fifth_die = rand(1..4)
-  sum = first_die + second_die + third_die + fourth_die
-  outcome = "You rolled a #{first_die} and a #{second_die} and a #{third_die} and a #{fourth_die} and a #{fifth_die} for a total of #{sum}."
+  sum = first_die + second_die + third_die + fourth_die + fifth_die
+  @outcome = "You rolled a #{first_die} and a #{second_die} and a #{third_die} and a #{fourth_die} and a #{fifth_die} for a total of #{sum}."
   	
-  "<h1>5d4</h1>
-   <p>#{outcome}</p>"
+  # "<h1>5d4</h1>
+  #  <p>#{outcome}</p>"
+   erb(:five_four, {:layout => :wrapper})
 end 
+
+
+get("/dice/100/6") do
+  @rolls = []    # Create a blank array
+
+  100.times do    # 100 times...
+    die = rand(1..6)    # Generate a random number
+    @rolls.push(die)    # Add the random number to the array 
+  end
+
+  erb(:one_hundred_six, {:layout => :wrapper})
+end
